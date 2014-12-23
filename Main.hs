330949b651e8a5fb39c1a6331aedae7d6620acd3
siamezzze <siamezzze@gmail.com>
2014-12-23 14:49:53 +0300

    Это уже похоже на правду, сливаю в master

--- Main.hs
+++ Main.hs
@@ -1,185 +1,202 @@
-module Main where
-
+{-# LANGUAGE TemplateHaskell, DeriveDataTypeable, DeriveGeneric, FlexibleInstances, UndecidableInstances #-}
+import Control.Distributed.Process
+import Control.Distributed.Process.Closure
+import Control.Distributed.Process.Node
+import Control.Distributed.Process.Backend.SimpleLocalnet
+import Control.Concurrent
+import Text.Printf
+import Data.Binary
+import Data.Typeable
+import GHC.Generics (Generic)
+import Control.Monad
+import System.Environment
+import qualified Data.Map.Strict as Map
 import Deck
-
-import Data.List
+import Game
 import System.Random
-import Control.Monad.State
+--Предлагаю по возможности все игровое взаимодействие сделать в модуле Game (нынешний Main) и здесь импортировать его
+--import Game
 
--- |'main' is the entry point for the program. This function will bind a
--- random number generator and pass it into a new 'Game' state. Finally, it will
--- evaluate the 'gameLoop'.
-main :: IO ()
+--типы сообщений
+data Msg = Ping ProcessId | Pong ProcessId | Name ProcessId String | Greetings ProcessId String | Play ProcessId String | Act ProcessId Int | Result ProcessId String | Results ProcessId String | Ask ProcessId String
+  deriving (Typeable, Generic)
+instance Binary Msg
+
+class (Binary a, Typeable a) => Serializable a
+instance (Binary a, Typeable a) => Serializable a
+
+
+type IDCards = Map.Map ProcessId String
+type Results = Map.Map ProcessId Int
+
+hasNames :: Map.Map ProcessId String -> Bool
+hasNames ids = all ( /= "") $ Map.elems ids
+
+hadPlayed :: Results -> Bool
+hadPlayed ids = all ( > 0) $ Map.elems ids
+
+playerActions :: Process () --
+playerActions = do
+    m <- expect
+    --liftIO (getLine) --подумать
+    case m of
+        Ping from -> do
+            say $ printf "ping received from %s" (show from) --Если пингуют, надо представиться
+            mypid <- getSelfPid
+            liftIO . putStrLn $ "What's your name?"
+            myname <- liftIO (getLine)
+            send from (Name mypid myname)
+            playerActions
+        Greetings from name -> do
+            liftIO . putStrLn $ "Greetings, " ++ name ++ "!"
+            playerActions
+        Play from game -> do --Здесь нужно выдать игроку информацию о состоянии игры
+            liftIO . putStrLn $ game
+            playerActions
+        Ask from st -> do --Спросить о его действиях
+            mypid <- getSelfPid
+            liftIO . putStrLn $ st
+            myaction <- liftIO (askForAction')
+            send from (Act mypid $ actionToInt myaction)
+            playerActions
+        Result from res -> do--Предварительный результат, т.е. рука, состояние, количество набранных очков
+            liftIO . putStrLn $ res
+            playerActions
+        Results from res -> do 
+            liftIO . putStrLn $ res
+            --Полная информация о набранных игроками и дилером очках
+        _ -> playerActions
+        
+
+remotable ['playerActions]
+
+
+master :: [NodeId] -> Process ()
+master peers = do
+    ps <- forM peers $ \nid -> do
+        say $ printf "spawning on %s" (show nid)
+        spawn nid $(mkStaticClosure 'playerActions)
+    mypid <- getSelfPid
+    forM_ ps $ \pid -> do
+        say $ printf "pinging %s" (show pid)
+        send pid (Ping mypid)
+    stdGen <- liftIO (newStdGen)
+    waitForNames stdGen $ Map.fromList (map (\pid -> (pid , "")) ps)
+    
+--Для образца
+waitForPongs :: [ProcessId] -> Process ()
+waitForPongs [ ] = return ()
+waitForPongs ps = do
+    m <- expect
+    case m of
+      Pong p -> waitForPongs (filter (/= p) ps)
+      Name p name -> do 
+        mypid <- getSelfPid
+        say $ "Name" ++ name ++ "recieved"
+        send p (Greetings mypid name)
+      _ -> say "MASTER received ping" >> terminate
+      
+
+--Знакомство
+waitForNames :: StdGen -> IDCards -> Process ()
+waitForNames g ids = 
+  if hasNames ids then do --Все поздоровались
+    say $ show ids
+    let pids = Map.keys ids
+    mypid <- getSelfPid
+    
+    --Создать новый экземпляр игры
+    stdGen <- liftIO (newStdGen)
+    let names = Map.mapKeys (show) ids
+    let game = makeGame names stdGen
+    let usersplaying = filter (\pid -> plays game (show pid)) pids
+    say $ show game
+    --playRound pids usersplaying game 
+    playRound pids [] game 
+    --waitForResults ps
+    return () 
+  else do
+    m <- expect
+    case m of
+      Name p name -> do --Здороваемся
+        mypid <- getSelfPid
+        say $ "Name " ++ name ++ " recieved"
+        send p (Greetings mypid name)
+        waitForNames g (Map.insert p name ids) --Запомнили имя, знакомимся дальше
+      _ -> say "Unexpected message" >> terminate
+
+--Игра
+broadcastInfo :: [ProcessId] -> Game -> Process ()
+broadcastInfo pids game = do --рассказать всем о состоянии игры
+  mypid <- getSelfPid
+  
+  forM_ pids $ \pid -> do
+    say $ printf "sending game %s" (show pid)
+    --Отправляем состояние игры
+    send pid (Play mypid (showGame game)) 
+    
+  let usersplaying = filter (\pid -> plays game (show pid)) pids --отбираем тех, кто играет в новом раунде
+  forM_ usersplaying $ \pid -> do
+    say $ printf "Asking for actions %s" (show pid)--Спрашиваем их о действиях
+    send pid (Ask mypid (playerInfo game (show pid))) 
+
+playRound :: [ProcessId] -> [ProcessId] -> Game -> Process ()
+playRound allusers [] game = do --раунд окончен
+  broadcastInfo allusers game --Отправляем _всем_ текущее состояние игры
+  let usersplaying = filter (\pid -> plays game (show pid)) allusers --отбираем тех, кто играет в новом раунде
+  if (anybodyPlays game) then playRound allusers usersplaying game --если такие еще есть - запускаем новый раунд
+  else playDealer allusers game --если таких нет - пора играть дилеру
+playRound allusers usersplaying game = do
+  mypid <- getSelfPid
+  m <- expect --ждем сообщения
+  case m of
+    Act from action -> --если сообщение о действии
+      if (from `elem` usersplaying) then do --от того, кто действовать может
+          let game' = applyAction game (show from) (actionFromInt action) --применим это действие
+          send from (Result mypid (playerInfo game' (show from))) --и отправим ему результат
+          playRound allusers (filter (/= from) usersplaying) game' --продолжим раунд, учтя, что он в этом раунде уже не играет
+      else
+          playRound allusers usersplaying game --от того, кто действовать не может - продолжаем раунд (игнорируем) 
+    _ -> playRound allusers usersplaying game --вообще непонятное сообщение - продолжаем раунд (игнорируем)
+    
+
+playDealer :: [ProcessId] -> Game -> Process ()
+playDealer pids game = return ()
+
+dealerRound :: [ProcessId] -> Game -> Process ()
+dealerRound = undefined
+
+sendResults :: [ProcessId] -> Game -> Process ()
+sendResults = undefined
+
+
+
+
+
+
+defaultHost = "127.0.0.1"
+defaultPort = "4242"
+
 main = do
-  stdGen <- newStdGen
-  evalStateT gameLoop $ mkGame stdGen hitUntilSixteen
-
-nDecks = 4
-
--- |The 'Game' data type contains all of the state information about a
--- blackjack game.
-data Game = Game
-  { deck :: Deck
-  , playerHand :: [Card]
-  , playerAction :: Action
-  , dealerHand :: [Card]
-  , dealerAction :: Action
-  , dealerStrategy :: Strategy }
-
-type GameState a = StateT Game a
-
--- |The 'Action' data type represents the possible actions a player can take.
-data Action = Hit | Stay deriving (Eq, Read)
-
--- |The 'Strategy' type is the signature of all AI functions used by the dealer.
-type Strategy = [Card] -> GameState IO (Action)
-
--- |'gameLoop' will repeatedly evaluate an iteration of the game. It updates the
--- game state based on the actions of the player and the dealer, and then
--- determines if the game is over.
-gameLoop :: GameState IO ()
-gameLoop = do
-  curr <- get
-  when ((playerAction curr) == Hit) handlePlayer
-  when ((dealerAction curr) == Hit) handleDealer
-  gameOver <- isGameOver
-  if gameOver then handleGameOver
-  else gameLoop
-
-
-
-playerInteraction curr = do
-    putStrLn $ "Your hand: " ++ (show $ playerHand curr)++ ", " ++ (show (possiblePoints (playerHand curr) ))
-    putStrLn $ "Dealer's hand: " ++ (showDealer $ dealerHand curr)
-    putStrLn "What do you want to do? (Hit/Stay)"
-    input <- getLine
-    return input
-
-handlePlayer :: GameState IO ()
-handlePlayer = do
-  curr <- get
-  input <- liftIO $ playerInteraction curr
-
-  let action = read input :: Action
-  when (action == Hit) $ do
-    let (card, deck') = runState draw $ deck curr
-    put curr { deck = deck'
-             , playerHand = card : playerHand curr }
-    new <- get
-    liftIO . putStrLn $ "Your hand: " ++ (show $ playerHand new)
-
-  when (action == Stay) $ do
-    put curr { playerAction = Stay }
-
-handleDealer :: GameState IO ()
-handleDealer = do
-  curr <- get
-  action <- dealerStrategy curr $ dealerHand curr
-  when (action == Hit) $ do
-    let (card, deck') = runState draw $ deck curr
-    put curr { deck = deck'
-             , dealerHand = card : dealerHand curr }
-    new <- get
-    let dealerH = dealerHand new
-    liftIO . putStrLn $ "The dealer hit."
-    liftIO . putStrLn $ "Dealer's hand: " ++ (showDealer dealerH)
-
-  when (action == Stay) $ do
-    put curr { dealerAction = Stay }
-    liftIO . putStrLn $ "The dealer stayed."
-
-isGameOver :: GameState IO Bool
-isGameOver = do
-  curr <- get
-  let playerA    = playerAction curr
-      dealerA    = dealerAction curr
-      playerH    = playerHand curr
-      dealerH    = dealerHand curr
-      bothStayed = (playerA == Stay && dealerA == Stay)
-      playerBust = bust playerH
-      dealerBust = bust dealerH
-      gameOver = bothStayed || playerBust || dealerBust
-
-  when playerBust $ liftIO . putStrLn $ "You busted out!"
-  when dealerBust $ liftIO . putStrLn $ "The dealer busted out!"
-
-  return gameOver
-
-handleGameOver :: GameState IO ()
-handleGameOver = do
-  curr <- get
-  let playerH = playerHand curr
-      dealerH = dealerHand curr
-      winner  = won playerH dealerH
-  liftIO . putStrLn $ "Your hand: " ++ (show playerH) ++ ", " ++ (show (possiblePoints playerH))
-  liftIO . putStrLn $ "Dealer's hand: " ++ (show dealerH) ++ ", " ++ (show (possiblePoints dealerH))
-  when winner $ liftIO . putStrLn $ "You win!"
-  when (not winner) $ liftIO . putStrLn $ "You lose..."
-
-won :: [Card] -> [Card] -> Bool
-won playerH dealerH = playerScore > dealerScore
-  where playerScore = score playerH
-        dealerScore = score dealerH
-
-score :: [Card] -> Int
-score h 
-  | bust h    = 0
-  | otherwise = best h
-
-bust :: [Card] -> Bool
-bust = and . map ((<) 21) . possiblePoints
-
-twentyOne :: [Card] -> Bool
-twentyOne = any ((==) 21) . possiblePoints
-
-sixteen :: [Card] -> Bool
-sixteen = any ((<=) 16) . possiblePoints
-
-best :: [Card] -> Int
-best = maximum . filter ((>=) 21) . possiblePoints
-
-showDealer :: [Card] -> String
-showDealer hand = "[" ++ (show $ head hand) ++ "," ++ (intersperse ',' hidden) ++ "]"
-  where n = length $ tail hand
-        hidden = replicate n '?'
-
-mkGame :: StdGen -> Strategy -> Game
-mkGame g strat = Game
-  { deck = d' 
-  , playerHand = playerH
-  , playerAction = Hit
-  , dealerHand = dealerH
-  , dealerAction = Hit
-  , dealerStrategy = strat }
-  where d = execState shuffle $ mkDeck g nDecks
-        ((playerH, dealerH), d') = runState deal $ d
-
-deal :: DeckState ([Card], [Card])
-deal = do
-  mine   <- draw
-  yours  <- draw
-  mine'  <- draw
-  yours' <- draw
-  let me = [mine, mine']
-      you = [yours, yours']
-  return (me, you)
-
-hitUnlessTwentyOne :: Strategy
-hitUnlessTwentyOne hand
-  | twentyOne hand = return Stay
-  | otherwise      = return Hit
-
-hitUntilSixteen :: Strategy
-hitUntilSixteen hand
-  | sixteen hand = return Stay
-  | otherwise      = return Hit
-
-hitSometimes :: Double -> Strategy
-hitSometimes threshold _ = do
-  curr <- get
-  let deck' = deck curr
-      (num, gen') = random $ gen deck'
-  put curr { deck = deck' { gen = gen' } }
-  if num > threshold
-    then return Hit
-    else return Stay
+    args <- getArgs
+    let rtable = Main.__remoteTable initRemoteTable
+    case args of
+        [ "master" ] -> do
+         backend <- initializeBackend defaultHost defaultPort rtable
+         startMaster backend master
+        [ "master" , host, port] -> do
+         backend <- initializeBackend host port rtable
+         startMaster backend master
+        [ "master", port ] -> do
+         backend <- initializeBackend defaultHost port rtable
+         startMaster backend master
+        [ "slave" ] -> do
+         backend <- initializeBackend defaultHost defaultPort rtable
+         startSlave backend
+        [ "slave", port ] -> do
+         backend <- initializeBackend defaultHost port rtable
+         startSlave backend
+        [ "slave", host, port ] -> do
+         backend <- initializeBackend host port rtable
+         startSlave backend
+        _ -> return ()
